44e6dd202a252a2f27ffe5e99047a7219175e22a
wolfgrey139 <alexdenny25@gmail.com>
2023-07-13 15:25:51 -0400

    Implemented primitive movement system. No collisions when interacting with the floor for some reason.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float exForce = 10f;
+    public float moveSpeed = 10f;
 
-    private Rigidbody2D rb;
+    Vector2 moveDir;
+    float horizontalInput;
+    float verticalInput;
+    Transform orientation;
+    
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +25,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movePlayer();
+    }
+
+    private void movePlayer()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+        //caculate movement
+        moveDir = (orientation.forward * verticalInput + orientation.right * horizontalInput) * Time.deltaTime;
+
+        rb.AddForce(moveDir * moveSpeed, ForceMode2D.Force);
     }
 
     // Explode
     // Main movement mechanic in the game, player inputs 
     // a direction for explosion to launch them
     // 
-    // 
-    public void Explode() {
-    
-    
-    
+    // Vector2 dir : direction of explosion force
+    public void Explode(Vector2 direction)
+    {
+        rb.AddForce(direction * exForce, ForceMode2D.Impulse);
     }
 
 }
