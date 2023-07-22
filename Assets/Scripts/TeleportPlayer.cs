b9e7fb843a26601aad984f58bde4e88225ed8b9b
wolfgrey139 <alexdenny25@gmail.com>
2023-07-22 12:34:29 -0400

    Fixed teleportation. Now bounds now teleport the player when they collide and reset tehir velocity to 0. This is just a placeholder so I can work on the movement mechanics without losing the player to the infinite void of space beyond the basic platform I setup

--- Assets/Scripts/TeleportPlayer.cs
+++ Assets/Scripts/TeleportPlayer.cs
@@ -6,27 +6,19 @@ public class TeleportPlayer : MonoBehaviour
 {
 
     public GameObject player;
-    //public BoxCollider2D boundsUp;
-    public BoxCollider2D boundsDown;
-    /*public BoxCollider2D boundsLeft;
-    public BoxCollider2D boundsRight;*/
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        //OnTriggerEnter2D(boundsUp);
-        OnTriggerEnter2D(boundsDown);
-        /*OnTriggerEnter2D(boundsLeft);
-        OnTriggerEnter2D(boundsRight);*/
-
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bounds")) {
-            Debug.Log("Teleport");
-            player.transform.position = new Vector2(0f, 0f);    
-        
+
+            //teleports player when they make contact with bounds
+            player.transform.position = new Vector2(0f, 0f);
+
+            //reset the player's velocity
+            Rigidbody2D playerRb = player.GetComponent<Rigidbody2D>();
+            playerRb.velocity = Vector2.zero;
+
         }
     }
 }
